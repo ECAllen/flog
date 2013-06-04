fb9b852e35cd8b635985321ad0d6fad94e12e904
Ethan Allen <ethancallen@gmail.com>
2013-06-04 14:05:24 +0000

    more web work

--- project.clj
+++ project.clj
@@ -3,4 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [ring "1.1.8"]
+                 [compojure "1.1.5"]
+                 [enlive "1.1.1"]
+                 [com.cemerick/friend "0.1.3"]]
+  :plugins [[lein-ring "0.8.3"]
+            [lein-marginalia "0.7.1"]]
+  :aot :all
+  :main flog.server
+  :ring {:handler flog.web/web}
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  :resources-path "public")
