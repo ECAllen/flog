ff8493e2649415a555ad92a63004bf689df21998
ethan <ethan@Vger.(none)>
2013-09-05 10:20:50 -0400

    reorganized the html templates added clutch and couchdb support

--- project.clj
+++ project.clj
@@ -3,17 +3,26 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.8"]
-                 [org.apache.commons/commons-email "1.2"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [ring "1.2.0"]
+                 [org.apache.commons/commons-email "1.3.1"]
                  [compojure "1.1.5"]
                  [org.clojure/tools.trace "0.7.5"]
-                 [enlive "1.1.1"]
-                 [com.cemerick/friend "0.1.3"]]
+                 [enlive "1.1.2"]
+                 [com.cemerick/friend "0.1.5"]
+                 [markdown-clj "0.9.29"]
+                 [com.ashafa/clutch "0.4.0-RC1"]
+                 [org.clojure/clojurescript "0.0-1450" :optional true
+                  :exclusions [com.google.code.findbugs/jsr305
+                               com.googlecode.jarjar/jarjar
+                               junit
+                               org.apache.ant/ant
+                               org.json/json
+                               org.mozilla/rhino]]]
   :plugins [[lein-ring "0.8.3"]
             [lein-marginalia "0.7.1"]]
   ;; :aot :all
   :main flog.core 
   :ring {:handler flog.core/site}
-  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
   :resources-path "public")
--- src/flog/core.clj
+++ src/flog/core.clj
@@ -1,7 +1,9 @@
 (ns flog.core
   (:use compojure.core
         ring.adapter.jetty
-        net.cgrand.enlive-html)
+        net.cgrand.enlive-html
+        flog.html
+        flog.blog)
   (:require [compojure.route :as route]
             [compojure.handler :as handler]
             [compojure.response :as response]
@@ -13,95 +15,38 @@
 (def user-file "src/flog/user.clj")
 (def users (ref (read-string (slurp user-file))))
 
-(def map-file "src/flog/user-map.clj")
-(def user-map (ref (read-string (slurp map-file))))
+;; ============================= 
+;; Config
+;; ============================= 
+;; (def menu-items (list "blog","tasks","lessons","resume","code-notes","projects","contact"))
 
+;; ============================= 
+;; Friend funcs
+;; ============================= 
+
+;; friend/authentication stuff
 (derive ::admin ::user)
 
-(defn lookup [usr] (spit "users.out" @users) (@users usr))
+;; function used by friend to lookup user for friend workflow
+;; (defn lookup [usr] (spit "users.out" @users) (@users usr))
 
-(defn tag [& {:keys [tag attrs content]
-              :or {tag "div" attrs nil content nil}}] 
-                {:tag (keyword tag) :attrs attrs :content (if (list? content) content (list content))})
-
-(def div-container 
-  (tag :attrs {:class "container-fluid" :id "container"}))
-
-;; NAVBAR ELEMENTS 
-(def menu-items '("blog","tasks","lessons","resume","code-notes","projects","contact"))
-
-(defn li-menu [menu]
-   (tag :tag "li" 
-        :content (tag :tag "a" 
-                      :attrs {:href (str "/" menu)} 
-                      :content (str menu))))
-
-(defn menu-list [] (map #(list %) menu-items))
-
-(def brand (tag :tag "a" 
-                :attrs {:class "brand" :href "#"} 
-                :content "Flog"))
-
-(def login-button (tag :tag "a" 
-                       :attrs {:class "btn btn-inverse pull-right" :href "/private"}
-                       :content "Login"))
-
-;; NAVBAR ASSEMBLY 
-(def navbar 
-  (->>  (list brand (doall (menu-list)) login-button)
-        (tag :attrs {:class "navbar-inner"} :content )
-        (tag :attrs {:id "nav" :class "navbar navbar-inverse"} :content )
-    ))
-
-
-;; HEAD ELEMENTS
-(def link-bootstrap (tag :tag "link" 
-                         :attrs {:href "bootstrap/css/bootstrap.min.css" 
-                                 :rel "stylesheet" 
-                                 :media "screen"}))
-
-(def script-jquery (tag :tag "script" 
-                        :attrs {:src "http://code.jquery.com/jquery.js"}))
-
-(def script-bootstrap (tag :tag "script" 
-                           :attrs {:src "bootstrap/js/bootstrap.min.js"}))
-
-;; (def login-form (at form [:form] (set-attr :class "form-horizontal")
-;;                          [:form] (append (at (div {:class "control-group"}) 
-;;                                               [:div] 
-;;                                               (append (at label [:label] (set-attr :class "control-label" :for "user")
-;;                                                                 [:label] (content "Name")))
-;;                                               [:div]
-;;                                               (append (at (div {:class "controls"}) 
-;;                                                           [:div]
-;;                                                           ;; ))))))
-;;                                                           (append (at input [:input] (set-attr :id "username" :type "text" :placeholder "username")))))))))
-;;
-
-;; HEAD ASSEMBLY
-(def head 
-  (->> (list link-bootstrap script-jquery script-bootstrap)
-       (tag :tag "head" :content )))
-
-;; BODY ASSEMBLY
-(def body 
-  (->> navbar
-       (tag :tag "body" :content )))
-
-;; HTML ASSEMBLY
-(def landing 
-  (->> (list head body)
-       (tag :tag "html"
-            :attrs {:lang "en"}
-            :content )))
+(defn lookup [usr] (@users usr))
 
 ;; ======================
 ;; page render
 ;; ======================
-
 (defn index []
-  (apply str (emit* landing)))
+  (apply str (emit* templt)))
 
+(defn blog-admin []
+  (apply str (emit* blog-admin-page)))
+
+(defn login []
+  (apply str (emit* login-page)))
+
+;; ======================
+;; Routes
+;; ======================
 (defroutes main-routes
   ;; ======================
   ;; authenticated routes
@@ -109,22 +54,30 @@
   (context "/private" request
            (friend/wrap-authorize
              (routes
-               (GET "/" [] (let [acctid (name ((keyword (str (:identity (friend/current-authentication)))) @user-map))]
-                             (apply str (index acctid))))
-               (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
-               )#{::user}))
+               (GET "/blogadmin" [] (blog-admin)) 
+               (POST "/add-blog-post" [md title]
+                     (post-blog title md (System/currentTimeMillis))
+                     (ring.util.response/redirect "/private/blogadmin"))
+
+               (friend/logout (ANY "/logout" request (ring.util.response/redirect "/"))))
+            #{::user}))
 
   ;; ======================
   ;; unauthenticated routes
   ;; ======================
+  ;; TODO make routes macro
   (GET "/" [] (index))
-;;   (GET "/login" [] (apply str (emit* login-tmpl)))
+  (GET "/blog" [] (index))
+  (GET "/login" [] (login))
   (route/files "")
   (route/not-found "404 baby"))
 
+;; friend authentication workflow
 (def site (handler/site
            (friend/authenticate main-routes
                                 {:credential-fn (partial creds/bcrypt-credential-fn lookup)
                                  :workflows [(workflows/interactive-form)]})))
+
+;; site handler
 (defn flog [routedef]
   (run-jetty routedef {:port 8080}))
